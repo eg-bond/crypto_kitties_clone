e51f0e1e8ad09d1ed32cd43f574f64684cdc9faf
eg-bond <eg-bond@yandex.ru>
2022-05-13 15:24:50 +0300

    begin stylize index page

--- client/new_frontend/src/pages/Index/IndexPage.js
+++ client/new_frontend/src/pages/Index/IndexPage.js
@@ -4,7 +4,15 @@ import './index_page.css'
 
 function IndexPage() {
   return (
-    <div className='mainIndexContainer'>
+    <div className='index'>
+      <div className='landing'>
+        <div className='landing__header'>
+          <h1>CryptoKittiesClone</h1>
+          <h3>Collect and breed furrever friends!</h3>
+        </div>
+        <img src='./images/group.png' alt='group' />
+        <button className='landing__btn'>Get your own kitty</button>
+      </div>
       <Link to='marketplace'>Get your own kitty</Link>
     </div>
   )
